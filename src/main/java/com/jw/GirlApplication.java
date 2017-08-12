5e294de15fbc92109dfce994f5259fc4eb1eed95
jw <jwnba24@163.com>
2017-08-12 20:41:47 +0800

    [task]12345677899是是是

--- src/main/java/com/jw/GirlApplication.java
+++ src/main/java/com/jw/GirlApplication.java
@@ -1,5 +1,6 @@
 package com.jw;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Created by Administrator on 2017/5/10.
  */
 @SpringBootApplication
+@MapperScan("com.jw.dao")
 public class GirlApplication {
     public static void main(String[] args) {
         SpringApplication.run(GirlApplication.class,args);
